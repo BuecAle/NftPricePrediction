83d18141df9c798eb6c2218a8d25f5305e55f76c
BuecAle <83408413+BuecAle1995@users.noreply.github.com>
2022-04-28 19:39:11 +0200

    - Cleaned up code

--- data/Data_Atomic/DataPrep_Atomic.py
+++ data/Data_Atomic/DataPrep_Atomic.py
@@ -6,16 +6,14 @@ import API_Atomic
 # Get Data from Atomic API
 # API_Atomic.Atomic_Data_Collection(2022-03-01, 2022-03-30)
 
-date = '1_2021'
+date = '2_2021'
 directory = 'C:/Users/BuecAle/Desktop/Uni/Masterthesis/NFT_Database/Data_Atomic/'
 filepath = directory + date + '/' + 'NFT_Atomic_' + date + '.csv'
 
-# Pfad auf Datum anpassen
+# Filter only atomicmarket nfts
 df = pd.read_csv(filepath)
 df = df[df["market_contract"].str.contains("atomicmarket")]
-df = df[["price","assets","seller"]]
-
-df
+df = df[["price", "assets", "seller"]]
 
 # WAX <-> USD value form "https://coinranking.com/de/coin/mWg3P2dAashSL+wax-wax"
 API_Atomic.get_price(df, "amount': '(.+?)'", "price_usd")
@@ -24,7 +22,7 @@ API_Atomic.get_expression(df, "collection_name': '(.+?)'", "collection_name")
 API_Atomic.get_data_expression(df, "name': '(.+?)'", "name")
 API_Atomic.get_data_expression(df, "img': '(.+?)'", "media")
 
-df = df[["asset_id","collection_name","name","price_usd","seller","media"]]
+df = df[["asset_id", "collection_name", "name", "price_usd", "seller", "media"]]
 df = df.dropna()
 df = df.drop_duplicates(subset=["asset_id"])
 df = df.drop_duplicates(subset=["name"])
